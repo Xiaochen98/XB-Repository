061f0bda1997a9e20c3317e2efa9b02886be3679
Xiaochen Bi <xiaochenbi98@gmail.com>
2024-04-08 21:44:18 +0800

    first submit

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉Correct Numer!';
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+let randomNumber = getRandomInt(1, 20);
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(randomNumber);
+  if (guess < randomNumber && guess > 0) {
+    document.querySelector('.message').textContent = 'Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > randomNumber) {
+    document.querySelector('.message').textContent = 'Too High';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess == randomNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number';
+    document.querySelector('.number').textContent = randomNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent =
+      'Please input number between 1-20';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Guess a number';
+  randomNumber = getRandomInt(1, 20);
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
